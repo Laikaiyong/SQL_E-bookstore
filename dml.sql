3c0055ba634a64c47c9d091b6c13bf06ce4dc9f8
Laikaiyong <vandycklai@gmail.com>
2021-06-08 14:18:18 +0800

    Finalized again

--- dml.sql
+++ dml.sql
@@ -1,3 +1,4 @@
+-- Lai Kai Yong
 -- 1.	List the book(s) which has the highest rating. Show book id, book name, and the rating.
 SELECT TOP 1 B.Book_ID, U.Book_Title, AVG(MF.Rating) AS Rating
 FROM BOOK B
@@ -32,6 +33,7 @@ GROUP BY M.Member_ID, M.Member_Name;
 
 
 -- 3.	Find the total number of book published by each publisher. Show publisher id, publisher name, and number of book published.
+-- Count all books published by publisher (Not only in bookstore)
 SELECT P.Pub_ID, P.Pub_Name, P.Pub_Qty
 FROM PUBLISHER P;
 
@@ -42,7 +44,7 @@ SELECT P.Pub_ID, P.Pub_Name, COUNT(B.Book_ID) AS 'Total book published'
 FROM BOOK B
 INNER JOIN PUBLISHER P
 ON B.Pub_ID = P.Pub_ID
-GROUP BY P.Pub_ID, P.Pub_Name
+GROUP BY P.Pub_ID, P.Pub_Name;
 
 -- 4.	Find the total number of books ordered by store manager from each publisher.
 SELECT P.Pub_ID, P.Pub_Name, SUM(BMO.Order_Quantity) AS 'Total Books Ordered'
@@ -54,10 +56,11 @@ ON BO.Order_ID = BMO.Order_ID
 GROUP BY P.Pub_ID, P.Pub_Name;
 
 
+-- Cheong Sheng Kui
 -- 5.	Find the total number of books ordered by each member.
 SELECT MO.Member_ID, SUM(BMO.MemOrder_Quantity) AS 'Total Books Ordered'
 FROM [MEMBER ORDER] MO INNER JOIN BOOK_MEMBER_ORDER BMO ON MO.MemOrder_ID = BMO.MemOrder_ID  
-GROUP BY MO.Member_ID
+GROUP BY MO.Member_ID;
 
 
 -- 6.   Find the bestselling book(s).
@@ -66,18 +69,19 @@ FROM BOOK_MEMBER_ORDER BMO INNER JOIN BOOK B ON BMO.Book_ID = B.Book_ID
 INNER JOIN UNIVERSAL_BOOK UB ON UB.Book_ISBN = B.Book_ISBN
 INNER JOIN [MEMBER ORDER] MO ON BMO.MemOrder_ID = MO.MemOrder_ID
 WHERE Payment_Status = 'Completed'
-GROUP BY BMO.Book_ID, UB.Book_Title ORDER BY SUM(BMO.MemOrder_Quantity) DESC
+GROUP BY BMO.Book_ID, UB.Book_Title ORDER BY SUM(BMO.MemOrder_Quantity) DESC;
 
 -- 7.   Show list of total customers based on gender who are registered as members in APU E-Bookstore. 
 --      The list should show total number of registered members and total number of gender (Male and female).
 SELECT COUNT(Member_ID) AS 'Total Registered Member',
 (SELECT COUNT(Member_ID) FROM MEMBER WHERE Gender = 'M') AS 'Male Member',
 (SELECT COUNT(Member_ID) FROM MEMBER WHERE Gender = 'F') AS 'Female Member'
-FROM MEMBER
+FROM MEMBER;
 
 
+-- Lim Wye Yee
 -- 8 Show a list of purchased books that have not been delivered to members. The list should show member identification number, address, contact number, book serial number, book title, quantity, date and status of delivery.
-SELECT DISTINCT M.Member_ID, CONCAT(M.Street, ', ', M.[Zip Code], ', ', M.City, ', ', M.State, ', ', M.Country) AS 'Member_Address', MCN.Personal_Phone, MCN.Work_Phone, B.Book_ID, UB.Book_Title, BMO.MemOrder_Quantity, MO.MemOrder_Date, MO.Delivery_Status
+SELECT DISTINCT M.Member_ID, M.Street, M.[Zip Code], M.City, M.State, M.Country, MCN.Personal_Phone, MCN.Work_Phone, B.Book_ID, UB.Book_Title, BMO.MemOrder_Quantity, MO.MemOrder_Date, MO.Delivery_Status
 FROM MEMBER M INNER JOIN MEMBER_CONTACT_NUMBER MCN ON M.Member_ID = MCN.Member_ID
 INNER JOIN [MEMBER ORDER] MO ON MO.Member_ID = M.Member_ID
 INNER JOIN BOOK_MEMBER_ORDER BMO ON BMO.MemOrder_ID = MO.MemOrder_ID
@@ -87,14 +91,14 @@ WHERE MO.Delivery_Status != 'Delivered';
 
 
 -- 9.	Show the member who spent most on buying books. Show member id, member name and total expenditure.
-SELECT M.Member_ID, M.Member_Name, MO.MemOrder_Price
+SELECT TOP 1 M.Member_ID, M.Member_Name, SUM(MO.MemOrder_Price) AS 'Total expenditure'
 FROM MEMBER M INNER JOIN [MEMBER ORDER] MO ON M.Member_ID = MO.Member_ID
-WHERE MO.MemOrder_Price = (SELECT MAX(MemOrder_Price) FROM [MEMBER ORDER]);
-
+GROUP BY M.Member_ID, M.Member_Name
+ORDER BY SUM(MO.MemOrder_Price) DESC;
 
 
 -- 10.	Show a list of total books as added by each members in the shopping cart.
-SELECT SC.Member_ID, M.Member_Name, COUNT(BSC.Purchase_Quantity) AS 'Total books purchased'
+SELECT SC.Member_ID, M.Member_Name, COUNT(BSC.Purchase_Quantity) AS 'Total books added'
 FROM [SHOPPING CART] SC INNER JOIN BOOK_SHOPPING_CART BSC ON SC.Purchase_ID = BSC.Purchase_ID
 INNER JOIN MEMBER M ON M.Member_ID = SC.Member_ID
 INNER JOIN BOOK B ON B.Book_ID = BSC.Book_ID
